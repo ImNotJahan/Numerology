1a16f86d15df79c1f82f747f2d6e8aeef3e14f7e
ImNotJahan <hiimjahan@gmail.com>
2023-09-04 18:09:34 -0700

    Added static generator

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
-import hashlib
+from hashlib import sha1
 from base64 import b64encode
+from random import getrandbits
 
 # a = 0, b = 1, c = 2 ... z = 25
 digits = {c: i for i, c in enumerate('abcdefghijklmnopqrstuvwxyz')}
@@ -13,4 +14,7 @@ def parse_number(number):
 
 # sha1 hashes a string and returns the hash in base 64
 def sha1_b64(text):
-    return b64encode(hashlib.sha1(text.encode()).digest()).decode()
+    return b64encode(sha1(text.encode()).digest()).decode()
+
+def rand_bool():
+    return bool(getrandbits(1))
---
+++ static.py
@@ -0,0 +1,52 @@
+'''
+Generates an image of random static containing a name
+'''
+
+from PIL import Image, ImageColor
+from common import parse_number, rand_bool
+
+width = int(input("Width? "))
+height = int(input("Height? "))
+
+# a new image is by default all black,
+# so we only need to add white
+white = ImageColor.getcolor('white', '1')
+
+str_name = input("Name? ")
+# converts to binary
+name = bin(parse_number(str_name))[2:]
+
+# one = white = true
+def parse_digit(string, index):
+    return string[index] == '1'
+
+name_length = len(name)
+
+found_name = False
+
+while not found_name:
+    # the '1' means that it's only black and white
+    img = Image.new('1', (width, height))
+    name_index = 0
+    
+    for y in range(0, height):
+        for x in range(0, width):
+            value = rand_bool()
+
+            # true = white
+            if(value):
+                img.putpixel((x, y), white)
+            # otherwise it's default black
+            
+            if not found_name:
+                if value == parse_digit(name, name_index):
+                    name_index += 1
+
+                    if(name_index == name_length):
+                        found_name = True
+                        print((x, y, name))
+
+                else:
+                    name_index = 0
+
+img.save(str_name + ".png")
